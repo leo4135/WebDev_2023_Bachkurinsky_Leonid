3ef4ae5095d319177d4d7b7c0d26f7bef978d226
User <leo4135@yandex.ru>
2023-04-08 10:10:47 +0300

    classes

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
-import 'uno.css'
-import "@unocss/reset/tailwind.css";
+import 'uno.css';
+import '@unocss/reset/tailwind.css';
+
+const domBtnCreateTask = document.getElementById('btnCreateTask');
+
+domBtnCreateTask.onclick = (e) => {
+  let modalWindowVisible = document.querySelector('.modalWindowVisible');
+  modalWindowVisible.classList.toggle('hidden');
+};
+
+const domBtnClose = document.querySelector('.domBtnClose');
+let modalWindowVisible = document.querySelector('.modalWindowVisible');
+domBtnClose.onclick = () => {
+  modalWindowVisible.classList.toggle('hidden');
+};
